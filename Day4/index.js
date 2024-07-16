e5e8164fa5ee5ce86a9a1d9da63c124960965ef7
yashksaini-coder <ys3853428@gmail.com>
2024-07-16 23:37:35 +0530

    chore: Activity 5 done! for Day-4

--- Day4/index.js
+++ Day4/index.js
@@ -105,3 +105,27 @@ console.log("Activity 5: ");
 
 // Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the `continue` statement.
 
+function skip(){{
+    for(let i=1; i<=10; i++){
+        if(i==5){
+            continue;
+        }
+        console.log(i);
+        }
+    }
+}
+
+skip();
+
+// Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the `break` statement.
+
+function stop(){
+    for(let i=1; i<=10; i++){
+        if(i==7){
+            break;
+        }
+        console.log(i);
+    }
+}
+
+stop();
