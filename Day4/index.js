4d4d0de7a0b797c414d6aa1f0c9526a8e8106285
yashksaini-coder <ys3853428@gmail.com>
2024-07-16 23:18:22 +0530

    chore: Activity 1 done! for Day-4

---
+++ Day4/index.js
@@ -0,0 +1,25 @@
+console.log("-------------------------------------------------");
+console.log("Activity 1: ");
+
+// Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+
+loop = () => {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+loop();
+
+// Task 2: Write a program to print the multiplication table of 5 using a for loop
+
+table = () => {
+    for(let i=0; i<=10; i++){
+        console.log(5*i);
+    }
+}
+
+table();
+
+console.log("-------------------------------------------------");
+console.log("Activity 2: ");
