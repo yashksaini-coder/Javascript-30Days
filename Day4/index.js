fcbeb0a8fe8edf06cda2415a10d89e6298e2281f
yashksaini-coder <ys3853428@gmail.com>
2024-07-16 23:38:31 +0530

    chore: All Activities done! for Day-4

--- Day4/index.js
+++ Day4/index.js
@@ -56,25 +56,28 @@ console.log("Activity 3: ");
 
 // Task 5: Write a program to print numbers from 1 to 5 using a do...while loop
 
-// let j = 1;
-// do {
-//     console.log(j);
-//     j++;
-// } while (j <= 5);
+function printNumbers(){
+    let i = 1;
+    do{
+        console.log(i);
+        i++;
+    }while(i<=5);
+}
 
+printNumbers();
 
 // Task 6: Write a program to calculate the factorial of a number using a do...while loop
 
-// factorial = (n) => {
-//     let fact = 1;
-//     let i = 1;
-//     do{
-//         fact *= i;
-//     }while(i<=n);
-//     console.log(fact);
-// }
+factorial = (n) => {
+    let fact = 1;
+    let i = 1;
+    do{
+        fact *= i;
+    }while(i<=n);
+    console.log(fact);
+}
 
-// factorial(5);
+factorial(5);
 
 console.log("-------------------------------------------------");
 console.log("Activity 4: ");
