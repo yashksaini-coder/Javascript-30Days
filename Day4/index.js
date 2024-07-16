c9087f7578156c0a859b57ee7bba75e80cb001d5
yashksaini-coder <ys3853428@gmail.com>
2024-07-16 23:21:31 +0530

    chore: Activity 2 done! for Day-4

--- Day4/index.js
+++ Day4/index.js
@@ -23,3 +23,38 @@ table();
 
 console.log("-------------------------------------------------");
 console.log("Activity 2: ");
+
+// Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+
+sum = () => {
+    let sum=0;
+    while(i<=10){
+        sum +=i;
+    }
+    console.log(sum);
+}
+
+sum();
+
+// Task 4: Write a program to print numbers from 10 to 1 using a while loop
+
+print = () => {
+    while(i<=10){
+        console.log(i);
+    }
+}
+
+print();
+
+console.log("-------------------------------------------------");
+console.log("Activity 3: ");
+
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 4: ");
+
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 5: ");
