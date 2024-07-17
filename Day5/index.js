bd767ae2545915251f41284d2f8f50fe4d6b8998
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-17 17:28:43 +0530

    Activity 1 completed of Day-5
    - [X] Task 1: Write a function to check if a number is even or odd and log the result to the console.
    - [X] Task 2: Write a function to calculate the square of a number and return the result.
    
    Co-Authored-By: Senior Dev'Codes <159620053+yashksaini19@users.noreply.github.com>

---
+++ Day5/index.js
@@ -0,0 +1,36 @@
+console.log("-------------------------------------------------");
+console.log("Activity 1: ");
+
+// Write a function to check if a number is even or odd and log the result to the console.
+function checker(num){
+    if (num % 2 ==0) console.log(num + " is even.");
+    else console.log(num + " is odd.");
+}
+
+checker(2);
+checker(3);
+
+// Task 2: Write a function to calculate the square of a number and return the result.
+
+function square(num){
+    console.log("Square of " + num + " is " + num*num);
+}
+
+square(2);
+square(3);
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 2: ");
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 3: ");
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 4: ");
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 5: ");
