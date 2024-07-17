14b50b08007e930a7788ae0dbe0b0f7639ad4f18
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-17 17:43:58 +0530

    Activity 2 completed of Day-5
    - [X] Task 3: Write a function expression to find the maximum of two numbers and log the result to the console.
    - [X] Task 4: Write a function expression to concatenate two strings and return the result.
    
    Co-Authored-By: Senior Dev'Codes <159620053+yashksaini19@users.noreply.github.com>

--- Day5/index.js
+++ Day5/index.js
@@ -23,6 +23,23 @@ square(3);
 console.log("-------------------------------------------------");
 console.log("Activity 2: ");
 
+// Task 3: Write a function expression to find the maximum of two numbers and log the result to the console.
+
+let max = function(num1, num2){
+    if (num1 > num2) console.log(num1 + " is greater than " + num2);
+    else console.log(num2 + " is greater than " + num1);
+}
+
+max(2,3);
+
+
+// Task 4: Write a function expression to concatenate two strings and return the result.
+
+let concat = function(str1, str2){
+    return str1 + " " + str2;
+}
+
+console.log(concat("Hello", "World"));
 
 console.log("-------------------------------------------------");
 console.log("Activity 3: ");
