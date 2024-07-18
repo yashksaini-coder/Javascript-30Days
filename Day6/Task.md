85d80394ccaa98bce2b64f45392e34e57fb75a04
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-18 13:22:05 +0530

    Activity 1 completed for Day 6

--- Day6/Task.md
+++ Day6/Task.md
@@ -16,8 +16,8 @@ This workshop is divided into several activities:
 
 ### Activity 1: Array Creation and Access 🔍
 
-- [ ] **Task 1:** Create an array of numbers from 1 to 5 and log the array to the console.
-- [ ] **Task 2:** Access the first and last elements of the array and log them to the console.
+- [X] **Task 1:** Create an array of numbers from 1 to 5 and log the array to the console.
+- [X] **Task 2:** Access the first and last elements of the array and log them to the console.
 
 ### Activity 2: Array Methods (Basic) 💡
 
---
+++ Day6/index.js
@@ -0,0 +1,34 @@
+console.log("-------------------------------------------------");
+console.log("Activity 1: ");
+
+// Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
+
+let arr = [1,2,3,4,5];
+console.log("The Array is:- "+arr);
+
+// Task 2:- Access the first and last elements of the array and log them to the console.
+console.log("The first element of the array is:- "+arr[0]);
+console.log("The last element of the array is:- "+arr[arr.length-1]);
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 2: ");
+
+
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 3: ");
+
+
+
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 4: ");
+
+
+
+
+console.log("-------------------------------------------------");
+console.log("Activity 5: ");
