769468fd68480308808bae75ef0042ebcd0e92c2
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-19 18:23:49 +0530

    Activity-1 completed for Day 7

---
+++ Day7/index.js
@@ -0,0 +1,16 @@
+console.log("-------------------------------------------------");
+console.log("Activity 1: ");
+
+// Task 1: Create an object representing a book with properties like `title`, `author`, and `year`, and log the object to the console.
+
+const book = {
+    title: "Rich Dad Poor Dad",
+    author: "Robert T Kiyosaki",
+    year: "2020"
+};
+console.log(book);
+
+// Task 2: Access and log the `title` and `author` properties of the book object.
+
+console.log("The title of the Book is:- " + book.title);
+console.log("The author of the book is:- " + book.author);
