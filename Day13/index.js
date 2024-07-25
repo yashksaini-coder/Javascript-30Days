5efd1f5eecf270bb2634965c1f2d2ff107742803
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-25 23:31:13 +0530

    Activity-2 completed for Day-13

--- Day13/index.js
+++ Day13/index.js
@@ -24,3 +24,29 @@ module.exports = person;
 
 console.log("-------------------------------------------------");
 console.log("Activity 2: ");
+
+// Task 3: Create a module that exports multiple functions using named exports. Import and use these functions in another script.
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+module.exports = {
+    multiply,
+    divide
+}
+
+// Task 4: Create a module that exports a single function using default export. Import and use this function in another script.
+
+function subtract(a, b) {
+    return a - b;
+}
+
+module.exports = subtract;
+
+console.log("-------------------------------------------------");
+console.log("Activity 3: ");
