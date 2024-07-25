f543fedd1fc8f072ce5381d58522f29988f5df26
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-25 23:29:40 +0530

    Activity-1 completed for Day-13

--- Day13/index.js
+++ Day13/index.js
@@ -1,2 +1,26 @@
 console.log("-------------------------------------------------");
 console.log("Activity 1: ");
+
+// Task 1: Create a module that exports a function to add two numbers. Import and use this module in another script.
+
+
+function add(a, b) {
+    return a + b;
+}
+
+module.exports = add;
+
+// Task 2: Create a module that exports an object representing a person with properties and methods. Import and use this module in another script.
+
+const person = {
+    name: "John",
+    age: 25,
+    greet: function() {
+        return "Hello, I am " + this.name;
+    }
+}
+
+module.exports = person;
+
+console.log("-------------------------------------------------");
+console.log("Activity 2: ");
