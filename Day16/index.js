64bc5447666fb3c9185d466e8a10c974df616821
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-08-07 21:54:05 +0530

    Chore: Completed Activity-2 for Day-16

--- Day16/index.js
+++ Day16/index.js
@@ -21,3 +21,24 @@ console.log(fibonacci(10)); // 55
 
 console.log("-------------------------------------------------");
 console.log("Activity 2: ");
+
+// Task 3: Write a recursive function to find the sum of all elements in an array. Log the result for a few test cases.
+
+function sumOfArray(arr, n) {
+    if (n <= 0) return 0;
+    return sumOfArray(arr, n - 1) + arr[n - 1];
+}
+
+console.log(sumOfArray([1, 2, 3, 4, 5], 5)); // 15
+
+// Task 4: Write a recursive function to find the maximum element in an array. Log the result for a few test cases.
+
+function maxOfArray(arr, n) {
+    if (n === 1) return arr[0];
+    return Math.max(arr[n - 1], maxOfArray(arr, n - 1));
+}
+
+console.log(maxOfArray([19, 22, 43, 84, 125], 5)); // 125
+
+console.log("-------------------------------------------------");
+console.log("Activity 3: ");
