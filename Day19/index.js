613ac8a747a29c1ff68227f317db213aa6f34cd2
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-08-10 20:29:27 +0530

    Chore: Activity-3 Completed for Day-19

--- Day19/index.js
+++ Day19/index.js
@@ -41,6 +41,22 @@ console.log(matches);
 console.log("-------------------------------------------------");
 console.log("Activity 3: ");
 
+// Task 5: Write a regular expression to capture the area code, central office code, and line number from a US phone number format (e.g., (123) 456-7890). Log the captures.
+
+pattern = /\((\d{3})\)\s(\d{3})-(\d{4})/;
+str = "(123) 456-7890";
+matches = str.match(pattern);
+
+console.log(matches);
+
+//  Task 6: Write a regular expression to capture the username and domain from an email address. Log the captures.
+
+pattern = /(\w+)@(\w+\.\w+)/;
+str = "yashsaini" + "@" + "gmail.com";
+let email = "yashkumarsaini0123456789@gmail.com";
+matches = email.match(pattern);
+
+console.log(matches);
 
 
 console.log("-------------------------------------------------");
