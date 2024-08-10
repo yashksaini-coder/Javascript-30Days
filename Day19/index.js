170ad2a14ea49abec48da36d0ad4a6e1c8cedcfe
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-08-10 20:27:35 +0530

    Chore: Activity-2 Completed for Day-19

--- Day19/index.js
+++ Day19/index.js
@@ -21,6 +21,21 @@ console.log(matches);
 console.log("-------------------------------------------------");
 console.log("Activity 2: ");
 
+// Task 3: Write a regular expression to match all words in a string that start with a capital letter. Log the matches.
+
+pattern = /[A-Z][a-z]+/g;
+str = "The quick brown fox Jumps over the lazy dog.";
+matches = str.match(pattern);
+
+console.log(matches);
+
+//  Task 4: Write a regular expression to match all sequences of one or more digits in a string. Log the matches.
+
+pattern = /\d+/g;
+str = "There are 12 months in a year.";
+matches = str.match(pattern);
+
+console.log(matches);
 
 
 console.log("-------------------------------------------------");
