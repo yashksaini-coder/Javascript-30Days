383cae9213257af409f4c39e35b50f35a0341c41
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-08-10 20:25:59 +0530

    Chore: Activity-1 Completed for Day-19

--- Day19/index.js
+++ Day19/index.js
@@ -1,6 +1,21 @@
 console.log("-------------------------------------------------");
 console.log("Activity 1: ");
 
+// Task 1: Write a regular expression to match a simple pattern (e.g., match all occurrences of the word "JavaScript" in a string). Log the matches.
+
+let pattern = /JavaScript/g;
+let str = "JavaScript is a scripting language. JavaScript is used for web development. JavaScript makes web pages interactive.";
+let matches = str.match(pattern);
+
+console.log(matches);
+
+//  Task 2: Write a regular expression to match all digits in a string. Log the matches.
+
+pattern = /\d/g;
+str = "There are 12 months in a year.";
+matches = str.match(pattern);
+
+console.log(matches);
 
 
 console.log("-------------------------------------------------");
