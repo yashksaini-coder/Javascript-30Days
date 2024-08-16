3fbc0796198889fdb62dc468f59727f8b878009c
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-08-16 15:04:40 +0530

    Chore: Activity-2 completed for Day-20

--- Day20/index.js
+++ Day20/index.js
@@ -21,13 +21,15 @@ const myObject = {
 };
 localStorage.setItem('myObjectKey', JSON.stringify(myObject));
 const retrievedObject = JSON.parse(localStorage.getItem('myObjectKey'));
-console.log(retrievedObject); // Output: { name: 'Yash', age: 24, profession: 'Software Engineer' }
+console.log(retrievedObject); 
 
 
 console.log("-------------------------------------------------");
 console.log("Activity 2: ");
 
-
+// Task 3: Create a simple form that saves user input (e.g., name and email) to localStorage when submitted. Retrieve and display the saved data on page load.
+// Task 4: Write a script to remove an item from localStorage. Log the localStorage content before and after removal.
+console.log("\nTask Completed!");
 
 console.log("-------------------------------------------------");
 console.log("Activity 3: ");
